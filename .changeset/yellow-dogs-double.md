86c6e6ac64ee789caa6633868d830719fab2fa1a
Arsh <69170106+lilnasy@users.noreply.github.com>
2023-11-13 20:46:26 +0000

    feat(global): automatically update env.d.ts (#3)

---
+++ .changeset/yellow-dogs-double.md
@@ -0,0 +1,5 @@
+---
+"astro-global": minor
+---
+
+The integration now automatically updates your project types to include the "astro:global" module.
--- packages/global/integration.ts
+++ packages/global/integration.ts
@@ -1,3 +1,4 @@
+import fs from "node:fs"
 import type { AstroConfig, AstroIntegration } from "astro"
 
 interface Options {}
@@ -6,24 +7,46 @@ export default function (options: Partial<Options> = {}): AstroIntegration {
     return {
         name: "astro-global",
         hooks: {
-            "astro:config:setup": ({ addMiddleware, updateConfig }) => {
+            "astro:config:setup": ({ addMiddleware, updateConfig, config, logger }) => {
                 addMiddleware({
                     entrypoint: "astro-global/runtime/middleware.ts",
                     order: "pre"
                 })
 
-                const config: Partial<AstroConfig> = {
+                const extraConfig: Partial<AstroConfig> = {
                     vite: {
                         plugins: [{
                             name: "astro-global",
                             resolveId(source) {
                                 if (source === "astro:global") return this.resolve("astro-global/runtime/virtual-module.ts")
                             }
+                        }, {
+                            name: "astro-global-inject-env-ts",
+                            enforce: "post",
+                            config() {
+                                const envTsPath = new URL("env.d.ts", config.srcDir)
+                                
+                                let typesEnvContents: string
+                                try { typesEnvContents = fs.readFileSync(envTsPath, "utf-8") }
+                                catch { return }
+
+                                if (typesEnvContents.includes('/// <reference types="astro-global/client" />')) { return }
+                                
+                                const newTypesEnvContents = typesEnvContents.replace(
+                                    '/// <reference types="astro/client" />',
+                                    '/// <reference types="astro/client" />\n/// <reference types="astro-global/client" />'
+                                )
+                                
+                                if (newTypesEnvContents === typesEnvContents) { return }
+
+                                fs.writeFileSync(envTsPath, newTypesEnvContents)
+                                logger.info("Updated env.d.ts types")
+                            }
                         }]
                     }
                 }
 
-                updateConfig(config)
+                updateConfig(extraConfig)
             }
         }
     }
