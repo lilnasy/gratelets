014c0860db97096d7197683b293f22d2b5359c1a
lilnasy <69170106+lilnasy@users.noreply.github.com>
2023-11-15 20:19:36 +0000

    rework tests

--- packages/scope/integration.ts
+++ packages/scope/integration.ts
@@ -1,12 +1,13 @@
 import fs from "node:fs"
-import type { AstroConfig, AstroIntegration } from "astro"
 import { cachedCompilation } from "./node_modules/astro/dist/core/compile/cache.js"
+import { parseAstroRequest } from "./node_modules/astro/dist/vite-plugin-astro/query.js"
+import type { AstroConfig, AstroIntegration } from "astro"
 
 interface Options {}
 
 export default function (options: Partial<Options> = {}): AstroIntegration {
     return {
-        name: "astro-global",
+        name: "astro-scope",
         hooks: {
             "astro:config:setup": ({ updateConfig, config, logger }) => {
                 updateConfig({
@@ -14,21 +15,33 @@ export default function (options: Partial<Options> = {}): AstroIntegration {
                         plugins: [{
                             name: "astro-scope",
                             resolveId(source, importer) {
-                                if (source === "astro:scope") return `astro:scope:${importer}.doesnotendwithtsdontprocessthispls`
+                                if (source !== "astro:scope") return
+
+                                if (importer === undefined) throw new Error("Vite's resolveId should never be called without an importer. This is an internal error. Please open an issue with reproduction steps.")
+                                
+                                const { filename } = parseAstroRequest(importer)
+                                
+                                if (filename.endsWith(".astro") === false) {
+                                    logger.error(`'astro:scope' was imported by ${importer}`)
+                                    throw new StacklessError("astro:scope can only be used in .astro files")
+                                }
+                                
+                                return `astro:scope:${filename}.doesnotendwithastrodontprocessthispls`
                             },
                             async load(id) {
-                                if (id.startsWith("astro:scope")) {
-                                    const importer = id.slice("astro:scope:".length, -".doesnotendwithtsdontprocessthispls".length)
-                                    console.log({ importer })
-                                    const result = await cachedCompilation({
-                                        astroConfig: config,
-                                        viteConfig: config.vite,
-                                        filename: importer,
-                                        source: (await this.load({ id: importer })).id!
-                                    })
-                                    // @ts-expect-error what?
-                                    return `export default ${JSON.stringify(result.scope)}`
-                                }
+                                if (!id.startsWith("astro:scope")) return
+
+                                const filename = id.slice("astro:scope:".length, -".doesnotendwithastrodontprocessthispls".length)
+                                
+                                const result = await cachedCompilation({
+                                    astroConfig: config,
+                                    viteConfig: config.vite,
+                                    filename,
+                                    source: fs.readFileSync(filename, "utf-8")
+                                })
+                                
+                                // @ts-expect-error what?!
+                                return `export default ${JSON.stringify(result.scope)}`
                             }
                         }, {
                             name: "astro-global-inject-env-ts",
@@ -40,16 +53,17 @@ export default function (options: Partial<Options> = {}): AstroIntegration {
                                 try { typesEnvContents = fs.readFileSync(envTsPath, "utf-8") }
                                 catch { return }
 
-                                if (typesEnvContents.includes('/// <reference types="astro-scope/client" />')) { return }
+                                if (typesEnvContents.includes('/// <reference types="astro-scope/client" />')) return
                                 
                                 const newTypesEnvContents = typesEnvContents.replace(
                                     '/// <reference types="astro/client" />',
                                     '/// <reference types="astro/client" />\n/// <reference types="astro-scope/client" />'
                                 )
                                 
-                                if (newTypesEnvContents === typesEnvContents) { return }
+                                if (newTypesEnvContents === typesEnvContents) return
 
                                 fs.writeFileSync(envTsPath, newTypesEnvContents)
+
                                 logger.info("Updated env.d.ts types")
                             }
                         }]
@@ -59,3 +73,6 @@ export default function (options: Partial<Options> = {}): AstroIntegration {
         }
     }
 }
+
+// the stacktrace is noisy and unhelpful 😠
+class StacklessError extends Error { stack = undefined }
--- packages/scope/tests/scope.test.ts
+++ packages/scope/tests/scope.test.ts
@@ -1,13 +1,24 @@
 import { describe, beforeAll, test, expect } from 'vitest'
 import { build, readTextFile } from './utils.ts'
 
+const resultRegex = /<script type="module">const o="(?<script>.{8})";console.log\(o\);\n<\/script><h1 data-astro-cid-(?<attribute>.{8})>(?<textContent>.{8})<\/h1>/
+
 describe('astro-scope', () => {
     beforeAll(async () => {
         await build('./fixtures/minimal')
     })
 
-    test('TODO', async () => {
-        const renderedScope = readTextFile('./fixtures/minimal/dist/index.html')
-        expect(renderedScope).to.toMatchInlineSnapshot('"<!DOCTYPE html>j7pv25f6"')
+    test('Page A', async () => {
+        const page = readTextFile('./fixtures/minimal/dist/page-a/index.html')
+        const { script, attribute, textContent } = page.match(resultRegex)!.groups!
+        expect(script).to.equal(attribute)
+        expect(attribute).to.equal(textContent)
+    })
+
+    test('Page A', async () => {
+        const page = readTextFile('./fixtures/minimal/dist/page-b/index.html')
+        const { script, attribute, textContent } = page.match(resultRegex)!.groups!
+        expect(script).to.equal(attribute)
+        expect(attribute).to.equal(textContent)
     })
 })
