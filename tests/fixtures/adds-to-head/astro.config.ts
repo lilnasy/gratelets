84b695092cbeaa198db4ec9f3db56623589289ea
Arsh <69170106+lilnasy@users.noreply.github.com>
2023-11-29 16:04:02 +0000

    chore: bump versions (#14)
    updates

--- tests/fixtures/adds-to-head/astro.config.ts
+++ tests/fixtures/adds-to-head/astro.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config"
 import mdx from "@astrojs/mdx"
 import addsToHead from "astro-adds-to-head"
 
+// https://astro.build/config
 export default defineConfig({
-    integrations: [ mdx(), addsToHead() ]
+    integrations: [mdx(), addsToHead()]
 })
--- tests/fixtures/prerender-patterns/astro.config.ts
+++ tests/fixtures/prerender-patterns/astro.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config"
 import prerenderPatterns, { prerender, renderOnDemand } from "astro-prerender-patterns"
 import { testAdapter } from "../../utils.ts"
 
-
+// https://astro.build/config
 export default defineConfig({
     output: "hybrid",
 	adapter: testAdapter,
