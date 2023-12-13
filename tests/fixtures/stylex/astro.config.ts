cc1e642f1c0c11688f5004fd1601bf7a46c0f32a
Arsh <69170106+lilnasy@users.noreply.github.com>
2023-12-13 13:51:23 +0000

    chore(stylex): add tests with solid (#36)

--- tests/fixtures/stylex/astro.config.ts
+++ tests/fixtures/stylex/astro.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config"
 import stylex from "astro-stylex"
 import react from "@astrojs/react"
+import solid from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [stylex(), react({ include: "**/react.tsx" })]
+    integrations: [stylex(), react({ include: "**/react.tsx" }), solid({ include: "**/solid.tsx" })]
 })
