29b91bf33b015a3ba343624ffea6a376aaa421cd
lilnasy <69170106+lilnasy@users.noreply.github.com>
2023-12-19 16:04:37 +0000

    chore: raise timeout for hono image test

--- tests/hono/image.test.ts
+++ tests/hono/image.test.ts
@@ -6,4 +6,4 @@ test("it can optimize and serve images", async ({ expect, server }) => {
     const resImage = await fetch("http://localhost:4321/_image?href=/_astro/some_penguin.9PDPbedL.png&w=50&f=webp")
     expect(resImage.status).to.equal(200)
     await server.destroy()
-})
+}, { timeout: 10000 })
