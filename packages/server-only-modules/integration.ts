2353e6e8a4d64b012c6dc314185e121b3d58b9ee
lilnasy <69170106+lilnasy@users.noreply.github.com>
2024-01-11 15:56:16 +0000

    fail on ts, mts, cts, tsx, js, mjs, cjs, jsx

--- packages/server-only-modules/integration.ts
+++ packages/server-only-modules/integration.ts
@@ -2,6 +2,9 @@ import type { AstroIntegration } from "astro"
 
 interface Options {}
 
+/**
+ * Prevents modules with the extension `.server.ts` from being imported into client-side code.
+ */
 export default function (_?: Options): AstroIntegration {
     let buildingFor: "server" | "client" | undefined = undefined
     return {
@@ -11,8 +14,19 @@ export default function (_?: Options): AstroIntegration {
                 updateConfig({ vite: { plugins: [{
                     name: "server-only-modules/vite",
                     load(specifier) {
-                        if (specifier.endsWith(".server.ts") && buildingFor === "client") {
-                            throw new Error(`Cannot import ${specifier} in the ${buildingFor} build.`)
+                        if (buildingFor === "client") {
+                            if (
+                                specifier.endsWith(".server.ts") ||
+                                specifier.endsWith(".server.mts") ||
+                                specifier.endsWith(".server.cts") ||
+                                specifier.endsWith(".server.tsx") ||
+                                specifier.endsWith(".server.js") ||
+                                specifier.endsWith(".server.mjs") ||
+                                specifier.endsWith(".server.cjs") ||
+                                specifier.endsWith(".server.jsx")
+                            ) {
+                                throw new ServerOnlyModule
+                            }
                         }
                     },
                 }] } })
@@ -21,5 +35,12 @@ export default function (_?: Options): AstroIntegration {
                 buildingFor = target
             }
         }
+    }
+}
+
+class ServerOnlyModule extends Error {
+    name = "ServerOnlyModule"
+    constructor() {
+        super(`Cannot import a server-only module in the client build.`)
     }
 }
