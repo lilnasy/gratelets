2c8ca9c9121080315dfc765d9986e2c16c88c0d6
lilnasy <69170106+lilnasy@users.noreply.github.com>
2024-02-04 15:19:29 +0000

    remove integration: astro-hono

--- packages/hono/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# astro-hono
-
-## 0.0.1
-
-### Patch Changes
-
-- [#23](https://github.com/lilnasy/gratelets/pull/23) [`e58f5cd`](https://github.com/lilnasy/gratelets/commit/e58f5cdc30826f22245355016ca8a0af6b36f485) Thanks [@lilnasy](https://github.com/lilnasy)! - Initial release
--- tests/hono/image.test.ts
+++
@@ -1,9 +0,0 @@
-import { testFactory } from "./utils.ts"
-
-const test = testFactory("./fixtures/hono/image/")
-
-test("it can optimize and serve images", async ({ expect, server }) => {
-    const resImage = await fetch("http://localhost:4321/_image?href=/_astro/some_penguin.9PDPbedL.png&w=50&f=webp")
-    expect(resImage.status).to.equal(200)
-    await server.destroy()
-}, { timeout: 10000 })
