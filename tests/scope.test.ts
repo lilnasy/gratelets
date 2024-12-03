4e7793e59b7fc29e8a95d93623d301a2891ed6e1
Arsh <69170106+lilnasy@users.noreply.github.com>
2024-12-03 15:32:19 +0000

    adjust tests: scope

--- tests/scope.test.ts
+++ tests/scope.test.ts
@@ -1,7 +1,7 @@
 import { describe, beforeAll, test, expect } from "vitest"
 import { build, type BuildFixture } from "./utils.ts"
 
-const resultRegex = /<script type="module">const o="(?<script>.{8})";console.log\(o\);\n<\/script><h1 data-astro-cid-(?<attribute>.{8})>(?<textContent>.{8})<\/h1>/
+const resultRegex = /<script type="module">const o="(?<script>.{8})";console.log\(o\);<\/script>\s<style>h1\[data-astro-cid-(?<style>.{8})]{color:red}\n<\/style><h1 data-astro-cid-(?<attribute>.{8})>(?<textContent>.{8})<\/h1>/
 
 describe("astro-scope", () => {
     let fixture: BuildFixture
@@ -12,6 +12,7 @@ describe("astro-scope", () => {
 
     test("Page A", async () => {
         const page = fixture.readTextFile("page-a/index.html")
+        console.log( page )
         const { script, attribute, textContent } = page.match(resultRegex)!.groups!
         expect(script).to.equal(attribute)
         expect(attribute).to.equal(textContent)
