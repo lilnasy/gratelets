253e7135e65e57c880bf35ee3e1ad4234f2c939f
Arsh <69170106+lilnasy@users.noreply.github.com>
2025-01-07 16:31:12 +0000

    remove unnecessary code path

--- packages/dynamic-import/integration.ts
+++ packages/dynamic-import/integration.ts
@@ -11,7 +11,7 @@ export default function (_?: Options): AstroIntegration {
     return {
         name: "astro-dynamic-import",
         hooks: {
-            "astro:config:setup" ({ config, updateConfig, logger }) {
+            "astro:config:setup" ({ config, updateConfig }) {
                 const srcDirName = path.relative(url.fileURLToPath(config.root), url.fileURLToPath(config.srcDir)).replaceAll("\\", "/")
                 updateConfig({ vite: { 
                   optimizeDeps: {
@@ -29,43 +29,8 @@ export default function (_?: Options): AstroIntegration {
                             `export const lookupMap = import.meta.glob('/${srcDirName}/components/**/*.astro', { query: { ${PROPAGATED_ASSET_FLAG}: true } })\n`
                         }
                     }
-                }, {
-                    name: "astro-dynamic-import/vite/types",
-                    enforce: "post",
-                    async config() {
-                        injectEnvDTS(config, logger, "astro-dynamic-import/client")
-                    }
-                }] } })
+                }]}})
             }
         }
     }
-}
-
-function injectEnvDTS(config: AstroConfig, logger: AstroIntegrationLogger, specifier: URL | string) {
-    const envDTsPath = url.fileURLToPath(new URL("env.d.ts", config.srcDir))
-    
-    if (specifier instanceof URL) {
-        specifier = url.fileURLToPath(specifier)
-        specifier = path.relative(url.fileURLToPath(config.srcDir), specifier)
-        specifier = specifier.replaceAll("\\", "/")
-    }
-    
-    let envDTsContents = fs.readFileSync(envDTsPath, "utf-8")
-    
-    if (envDTsContents.includes(`/// <reference types='${specifier}' />`)) { return }
-    if (envDTsContents.includes(`/// <reference types="${specifier}" />`)) { return }
-    
-    const newEnvDTsContents = envDTsContents.replace(
-        `/// <reference types='astro/client' />`,
-        `/// <reference types='astro/client' />\n/// <reference types='${specifier}' />\n`
-    ).replace(
-        `/// <reference types="astro/client" />`,
-        `/// <reference types="astro/client" />\n/// <reference types="${specifier}" />\n`
-    )
-    
-    // the odd case where the user changed the reference to astro/client
-    if (newEnvDTsContents === envDTsContents) { return }
-    
-    fs.writeFileSync(envDTsPath, newEnvDTsContents)
-    logger.info("Updated env.d.ts types")
 }
