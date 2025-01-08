4d804674497c2e3356675e3931b4e48640683edc
Arsh <69170106+lilnasy@users.noreply.github.com>
2025-01-07 22:08:08 -0500

    compile declaration file

---
+++ packages/dynamic-import/integration.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Adds the ability to dynamically import components, while
+ * including scripts and styles of only the picked components.
+ * @param {{}=} _ Not used, the integration does not have any configuration options
+ * @returns {import("astro").AstroIntegration}
+ */
+export default function _default(_?: {} | undefined): import("astro").AstroIntegration;
--- packages/dynamic-import/integration.js
+++ packages/dynamic-import/integration.js
@@ -1,13 +1,16 @@
+// @ts-check
+/// <reference path="./types.d.ts" />
 import url from "node:url"
 import path from "node:path"
-/** @import "/types.d.ts" */
 
 // ./node_modules/astro/dist/core/index.js
 const astroEntry = import.meta.resolve("astro")
-const consts = new URL("../../dist/content/consts.js", astroEntry)
-const { PROPAGATED_ASSET_FLAG } = await import(consts)
+const consts = new URL("../content/consts.js", astroEntry)
+const { PROPAGATED_ASSET_FLAG } = await import(consts.href)
 
 /**
+ * Adds the ability to dynamically import components, while
+ * including scripts and styles of only the picked components.
  * @param {{}=} _ Not used, the integration does not have any configuration options
  * @returns {import("astro").AstroIntegration}
  */
@@ -17,24 +20,26 @@ export default function (_) {
         hooks: {
             "astro:config:setup"({ config, updateConfig }) {
                 const srcDirName = path.relative(url.fileURLToPath(config.root), url.fileURLToPath(config.srcDir)).replaceAll("\\", "/")
+                /** @type {import("vite").Plugin} */
+                const plugin = {
+                    name: "astro-dynamic-import/vite",
+                    resolveId(source) {
+                        if (source === "astro:import") { return this.resolve("astro-dynamic-import/runtime/virtual-module.ts") }
+                        if (source === "astro-dynamic-import:internal") return source
+                    },
+                    load(id) {
+                        if (id === "astro-dynamic-import:internal") {
+                            return `export const srcDirName = ${JSON.stringify(srcDirName)}\n` +
+                                `export const lookupMap = import.meta.glob('/${srcDirName}/components/**/*.astro', { query: { ${PROPAGATED_ASSET_FLAG}: true } })\n`
+                        }
+                    }
+                }
                 updateConfig({
                     vite: {
                         optimizeDeps: {
-                            exclude: ["astro-dynamic-import:internal"],
+                            exclude: [ "astro-dynamic-import:internal" ],
                         },
-                        plugins: [{
-                            name: "astro-dynamic-import/vite",
-                            resolveId(source) {
-                                if (source === "astro:import") { return this.resolve("astro-dynamic-import/runtime/virtual-module.ts") }
-                                if (source === "astro-dynamic-import:internal") return source
-                            },
-                            load(id) {
-                                if (id === "astro-dynamic-import:internal") {
-                                    return `export const srcDirName = ${JSON.stringify(srcDirName)}\n` +
-                                        `export const lookupMap = import.meta.glob('/${srcDirName}/components/**/*.astro', { query: { ${PROPAGATED_ASSET_FLAG}: true } })\n`
-                                }
-                            }
-                        }]
+                        plugins: [ plugin ]
                     }
                 })
             }
