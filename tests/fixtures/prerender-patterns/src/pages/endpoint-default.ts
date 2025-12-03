b426186a40fb224ed8310e13dd74b18ae01b2cfa
Arsh <hi@arsh.sh>
2025-12-03 18:43:40 +0000

    chore(prerender patterns): remove package

--- tests/fixtures/prerender-patterns/src/pages/endpoint-default.ts
+++
@@ -1,3 +0,0 @@
-import type { APIRoute } from "astro"
-
-export const GET: APIRoute = ctx => new Response(ctx.request.url)
--- tests/fixtures/prerender-patterns/src/pages/endpoint-set-to-render-on-demand.ts
+++
@@ -1,5 +0,0 @@
-import type { APIRoute } from "astro"
-
-export const prerender = false
-
-export const GET: APIRoute = ctx => new Response(ctx.request.url)
